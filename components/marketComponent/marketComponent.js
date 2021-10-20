c3a99a3ab903ff6bf6b381e0142ab897fa4e5fd0
Jesus Lara <jelara99@gmail.com>
2021-10-19 19:17:06 -0600

    grafico completo

--- components/marketComponent/marketComponent.js
+++ components/marketComponent/marketComponent.js
@@ -1,19 +1,15 @@
-
+import TradingViewWidget, { Themes } from 'react-tradingview-widget';
 export default function MarketComponent() {
-    const getData = () => {
-        const graphicInfo = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
-
-        graphicInfo.onmessage = (message) => {
-            console.log(message.data)
-        }
-    }
-
- 
 
     return (
         <div className="row justify-content-center">
-            <div className="col-11 col-lg-8 mt-5 grapic">
-                hola
+            <div className="col-12 grapic">
+                <TradingViewWidget
+                    symbol="COINBASE:BTCUSD"
+                    theme={Themes.DARK}
+                    locale="es"
+                    autosize
+                />
             </div>
         </div>
     )
