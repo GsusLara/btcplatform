acda07f07ae9f1ce339f5c922e7b90c40911189f
Jesús Lara <jelara99@gmail.com>
2023-12-27 16:24:56 -0600

    botones

--- components/inicio/inicio.js
+++ components/inicio/inicio.js
@@ -1,33 +1,35 @@
 import { useContext } from "react"
 import { Context } from "../../store/appContext";
+import Link from "next/link"
 
 export default function Inicio() {
     const { store } = useContext(Context);
 
     return (
         <>
-            <div className="container-fluid pt-5 boxheader" id="top">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-8 text-center position-absolute">
-                            <p>
-                                <span className="fs-1 title">Btc en Costa Rica</span>
-                                <br />
-                                <span className="fs-3 subtitle">Compre y venda Bitcoin de forma rapida y segura </span>
-                            </p>
-                            <p>
-                                <span className="fs-5 subtitle">El Precio del BTC en este momento es de:</span>
-                                <br />
-                                <span className="fs-4 title">₡{new Intl.NumberFormat().format(store.BtcColon)}</span>
-                                <span className="fs-5 subtitle"> Colones, </span>
-                                <span className="fs-4 title">${new Intl.NumberFormat().format(store.BtcDolar)}</span>
-                                <span className="fs-5 subtitle"> Dolares</span>
-                            </p>
-                            <div className="botones-container">
-                                <button className="btn btn-primary btn-comprar mx-1">Comprar</button>
-                                <button className="btn btn-success btn-vender mx-1">Vender</button>
-                            </div>
+            <div className="container-fluid py-5 boxheader position-relative">
+                <div className="col-11 text-center position-absolute top-50 start-50 translate-middle">
+                    <p className="title fs-1 mb-5">Compre y venda Bitcoin de forma rapida y segura </p>
+                    <div className="m-lg-5">
+                        <p>
+                            <span className="fs-5 subtitle">Precio del BTC a las HORA:</span>
+                            <br />
+                            <span className="fs-4 title">₡{new Intl.NumberFormat().format(store.BtcColon)}</span>
+                            <span className="fs-5 subtitle"> Colones</span>
+                            <br />
+                            <span className="fs-4 title">${new Intl.NumberFormat().format(store.BtcDolar)}.00</span>
+                            <span className="fs-5 subtitle"> Dolares</span>
+                        </p>
+                        <div className="botones-container">
+                            <button className="btn btn-primary btnInicio btn-comprar mx-3">Comprar</button>
+                            <button className="btn btn-success btnInicio btn-vender mx-3">Vender</button>
                         </div>
+                        <p className="fs-3 title mt-5">Análiza tu mejor opción</p>
+                        
+                        <Link href="/Market">
+                        <button className="btn btn-primary btnInicio btn-trade">Trade-view</button>
+                        </Link>
+                            
                     </div>
                 </div>
             </div>
